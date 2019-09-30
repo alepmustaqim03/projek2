7feb0c52c82812340b139f68a46938e3149ad59f
user <user@LAPTOP-D1MK6H7D>
2019-09-30 23:02:49 +0800

    30/9/2019:lab_array

---
+++ src/lab_for/Lab_for.java
@@ -0,0 +1,13 @@
+
+package lab_for;
+public class Lab_for {
+
+    public static void main(String[] args) {
+       for (int w=1;w<=3;w++){
+       for (int x=1; x<=5;x++)
+           System.out.print("*");
+           System.out.println("");
+       }
+    }
+    
+}
